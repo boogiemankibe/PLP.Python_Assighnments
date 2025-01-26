cbbc1a7cd4cf51f7c13ff4aa8fa4617266f73694
boogiemankibe <antokimeli18@gmail.com>
2025-01-26 10:57:16 +0300

    my first python assighnment

---
+++ arithimetic.py
@@ -0,0 +1,11 @@
+num_1=float(input('input first numbers:'))
+num_2=float(input('input your second number:'))
+num_sign=input('input the arithimetic sighn you want')
+if num_sign == '+':
+  print(num_1+num_2)
+if num_sign == '-':
+  print(num_1-num_2)
+if num_sign == '*':
+  print(num_1*num_2)
+if num_sign == '/':
+  print(num_1/num_2)
